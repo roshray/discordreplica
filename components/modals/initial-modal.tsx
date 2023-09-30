18e48b9f7986a9336471958221e228949c2f7635
roshray <roshanroy759@gmail.com>
2023-09-30 16:49:20 +0530

    upload image, render image & remove button

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -26,6 +26,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button} from "@/components/ui/button"
 import { useEffect, useState } from "react"
+import {FileUpload} from "@/components/file-upload"
 
 const formSchema = z.object({
     name: z.string().min(1,{
@@ -79,7 +80,24 @@ export const InitialModal = () => {
                 >
                     <div className="space-y-8 px-6">
                         <div className="flex items-center justify-center text-center">
-                            TODO: Image Upload
+                            <FormField 
+                                control={form.control}
+                                name="imageUrl"
+                                render={({ field}) => (
+                                  <FormItem>
+                                    <FormControl>
+                                        <FileUpload 
+                                            endpoint="serverImage"
+                                            value={field.value}
+                                            onChange={field.onChange}
+
+                                        
+                                        />
+                                    </FormControl>
+                                  </FormItem>  
+                                )}
+                            
+                            />
                         </div>
 
                         <FormField
