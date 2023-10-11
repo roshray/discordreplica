50aea84becfa8688591af9011225e11e1573019d
roshray <roshanroy759@gmail.com>
2023-10-11 13:26:35 +0530

    Server Sidebar[Header]

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { ServerHeader } from "./server-header";
 
 interface ServerSidebarProps {
     serverId: string;
@@ -50,7 +51,27 @@ export const ServerSidebar =async ({
         (channel) => channel.type === ChannelType.VIDEO
         
     )
+
+    const members = server?.members.filter((member) => member.profileId !== profile.id)
+    
+    if (!server) {
+        return redirect("/")
+    }
+
+    const role = server.members.find((member) => member.profileId === profile.id)?.role
+    
+
+
+
+
+
     return (
-        <div className="">Server Sidebar Component</div>
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+            <ServerHeader 
+                server={server}
+                role={role}
+            
+            />
+        </div>
     )
 }
