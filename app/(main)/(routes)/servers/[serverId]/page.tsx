7fa36afe2f9e4071b57859af83d5eb6739bea62e
roshray <roshanroy759@gmail.com>
2023-11-17 17:30:35 +0530

    Channel Id Page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,46 @@
-const ServerIdPage = () => {
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+}
+const ServerIdPage =  async ({
+    params
+}: ServerIdPageProps) => {
+    const profile = await currentProfile()
+    if (!profile) {
+        return redirectToSignIn()
+    }
     
-    return (
-        <div className="">
-            Server Id Page
-        </div>
-    )
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    })
+
+    const initialChannel = server?.channels[0]
+
+    if (initialChannel?.name !== "general") {
+        return null
+    }
+    return (`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 export default ServerIdPage;
