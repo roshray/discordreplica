288b4ca063282dd04f46d711fb61825a843d14fb
roshray <roshanroy759@gmail.com>
2023-12-16 18:35:01 +0530

    useChatQuery: hooks

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -16,6 +16,15 @@ export const ChatWelcome = ({
                     <Hash className="h-12 w-12 text-white"/>
                 </div>
             )}
+            <p className="text-xl md:text-3xl font-bold">
+                { type === "channel" ? "Welcome to #" : ""}{name}
+            </p>
+            <p className="text-zinc-600 dark:text-zinc-400 text-sm">
+                {type === "channel"
+                    ? `This is the start of the #${name} channel.`
+                    : `This is the start of your conversation with ${name}`
+                }
+            </p>
         </div>
     )
 }
